93d2074e49f1e6a256c58c991292a41af6073015
gabs <146789686+ldrgabryele@users.noreply.github.com>
2023-12-11 14:15:06 -0300

    atividade31_exercicio.02f

---
+++ exercicio02-f.py
@@ -0,0 +1,35 @@
+#Crie um programa que leia dois números e mostre a soma, a
+#subtração, a multiplicação e a divisão entre eles]
+
+n1= int(input("Digite o 1 número: "))
+n2= int(input("Digite o 2 número: "))
+
+n3= int(input("Digite o 1 número: "))
+n4= int(input("Digite o 2 número: "))
+n5= int(input("Digite o 1 número: "))
+n6= int(input("Digite o 2 número: "))
+
+def soma (n1,n2):
+    soma = n1 + n2
+    print(soma)
+
+soma(n1,n2)
+
+def subtração (n1,n2):
+    sub = n1 - n2
+    print(sub)
+subtração(n1,n2)
+
+def multiplicação (n1,n2):
+    mult = n1 * n2
+    print(mult)
+multiplicação(n1,n2)
+
+def divisão (n1,n2):
+    div = n1 / n2
+    print(div)
+divisão(n1,n2)
+
+soma (n5,n2)
+
+subtração(n1, n3)
